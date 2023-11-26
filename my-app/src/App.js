379723391aba408160cdde12a8891f6f99171618
BrayanSO <briansolera@gmail.com>
2023-11-26 11:27:37 +0000

    backgraund

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <h1> B.Solera</h1>
+      <div className="button">
+      <button className="ON">ON</button>
+      <button className="OFF">OFF</button>
+      </div>
     </div>
   );
 }
