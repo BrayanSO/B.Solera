0a515f11b736087c5411d706099f2efa1a582f70
BrayanSO <briansolera@gmail.com>
2023-12-22 21:06:33 +0000

    prueba

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./Style/App.css";
+import "./Style/buttons.css";
 
 function App() {
   return (
     <div className="App">
       <h1> B.Solera</h1>
-      <div className="button">
+    <div>
       <button className="ON">ON</button>
       <button className="OFF">OFF</button>
       </div>
