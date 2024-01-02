32b5953e27fff74d3219b60512b879e6fb0293e2
BrayanSO <briansolera@gmail.com>
2024-01-02 23:00:09 +0000

    fix code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,174 +1,22 @@
-import React, { useEffect } from 'react';
-import "./Style/gosth.css";
+import React from 'react';
+import TextAnimation from './components/TextAnimation';
+import ConsoleAnimation from "./components/ConsoleAnimation"
+import "./Style/TextAnimation.css";
 import "./Style/buttons.css";
-import "./Style/text.css";
+import "./Style/ConsoleAnimation.css";
 
-const App = () => {
-  useEffect(() => {
-    consoleText(['Hello World' , 'console.log' , 'border:20px' , 'git add ..',], 'text', ['tomato', 'rebeccapurple', 'lightblue']);
-
-    const elts = {
-      text1: document.getElementById("text1"),
-      text2: document.getElementById("text2")
-    };
-
-    const texts = [
-      "React",
-      "HTML5 / CSS3",
-      "Javascrip",
-      "React",
-      "Python",
-      "Flux",
-      "API's",
-      "SQL",
-    ];
-
-    const morphTime = 1;
-    const cooldownTime = 0.25;
-
-    let textIndex = texts.length - 1;
-    let time = new Date();
-    let morph = 0;
-    let cooldown = cooldownTime;
-
-    elts.text1.textContent = texts[textIndex % texts.length];
-    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-
-    function doMorph() {
-      morph -= cooldown;
-      cooldown = 0;
-
-      let fraction = morph / morphTime;
-
-      if (fraction > 1) {
-        cooldown = cooldownTime;
-        fraction = 1;
-      }
-
-      setMorph(fraction);
-    }
-
-    function setMorph(fraction) {
-      elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-      elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
-      fraction = 1 - fraction;
-      elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-      elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
-      elts.text1.textContent = texts[textIndex % texts.length];
-      elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-    }
-
-    function doCooldown() {
-      morph = 0;
-
-      elts.text2.style.filter = "";
-      elts.text2.style.opacity = "100%";
-
-      elts.text1.style.filter = "";
-      elts.text1.style.opacity = "0%";
-    }
-
-    function animate() {
-      requestAnimationFrame(animate);
-
-      let newTime = new Date();
-      let shouldIncrementIndex = cooldown > 0;
-      let dt = (newTime - time) / 1000;
-      time = newTime;
-
-      cooldown -= dt;
-
-      if (cooldown <= 0) {
-        if (shouldIncrementIndex) {
-          textIndex++;
-        }
-
-        doMorph();
-      } else {
-        doCooldown();
-      }
-    }
-
-    animate();
-  }, []);
-
-  function consoleText(words, id, colors) {
-    if (colors === undefined) colors = ['#fff'];
-    var visible = true;
-    var con = document.getElementById('console');
-    var letterCount = 1;
-    var x = 1;
-    var waiting = false;
-    var target = document.getElementById(id);
-    target.setAttribute('style', 'color:' + colors[0]);
-    window.setInterval(function () {
-      if (letterCount === 0 && waiting === false) {
-        waiting = true;
-        target.innerHTML = words[0].substring(0, letterCount);
-        window.setTimeout(function () {
-          var usedColor = colors.shift();
-          colors.push(usedColor);
-          var usedWord = words.shift();
-          words.push(usedWord);
-          x = 1;
-          target.setAttribute('style', 'color:' + colors[0]);
-          letterCount += x;
-          waiting = false;
-        }, 1000);
-      } else if (letterCount === words[0].length + 1 && waiting === false) {
-        waiting = true;
-        window.setTimeout(function () {
-          x = -1;
-          letterCount += x;
-          waiting = false;
-        }, 1000);
-      } else if (waiting === false) {
-        target.innerHTML = words[0].substring(0, letterCount);
-        letterCount += x;
-      }
-    }, 120);
-    window.setInterval(function () {
-      if (visible === true) {
-        con.className = 'console-underscore hidden';
-        visible = false;
-      } else {
-        con.className = 'console-underscore';
-        visible = true;
-      }
-    }, 400);
-  }
-
+const Header = () => {
   return (
-< div className="App">
+    <div>
+      <TextAnimation></TextAnimation>
       <h1> B.Solera</h1>
-      <div className='console-containertext'>
-        <span id='text'></span><div className='console-underscore' id='console'>&#95;</div>
-        <div id="containertext">
-          <span id="text1"></span>
-          <span id="text2"></span>
-    </div>
-      </div>
-        <div>
+      <ConsoleAnimation></ConsoleAnimation>
+      <div>
         <button className="ON">ON</button>
         <button className="OFF">OFF</button>
       </div>
-
-        <div>
-        <svg id="filters">
-          <defs>
-            <filter id="threshold">
-              <feColorMatrix in="SourceGraphic" type="matrix" values="1 0 0 0 0
-                  0 1 0 0 0
-                  0 0 1 0 0
-                  0 0 0 255 -140" />
-            </filter>
-          </defs>
-        </svg>
-        </div>
-   </div>
+    </div>
   );
 }
 
-export default App;
+export default Header;
